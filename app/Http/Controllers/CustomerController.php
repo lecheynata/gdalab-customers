6624811c1ee26e1e3903d86caf46b27e91d62e83
eduardo mena <dev@lecheynata.com>
2022-06-26 14:45:28 -0500

    save new customer

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -6,5 +6,23 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $customer = new Customer;
+        $customer->dni = $request->dni;
+        $customer->id_reg = $request->id_reg;
+        $customer->id_com = $request->id_com;
+        $customer->email = $request->email;
+        $customer->name = $request->name;
+        $customer->last_name = $request->last_name;
+        $customer->address = $request->address;
+
+        return $customer->save();
+    }
 }
