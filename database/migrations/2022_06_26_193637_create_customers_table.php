17e810a45c0c69de61df332680f9f985ef6a6ff2
eduardo mena <dev@lecheynata.com>
2022-06-26 14:38:29 -0500

    create customers migration

---
+++ database/migrations/2022_06_26_193637_create_customers_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->string('dni', 45);
+            $table->integer('id_reg');
+            $table->integer('id_com');
+            $table->string('email', 120)->unique();
+            $table->string('name', 45);
+            $table->string('last_name', 45);
+            $table->string('address', 255)->nullable();
+            $table->timestamp('date_reg', $presicion = 0)->default(now());
+            $table->enum('status', ['A', 'I', 'trash'])->default('A');
+
+            $table->index('dni');
+            $table->primary(['dni', 'id_reg', 'id_com']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
