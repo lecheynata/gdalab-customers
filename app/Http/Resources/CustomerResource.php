c3be61d8dd8c5be427b8caa4164c325f1da50755
eduardo mena <dev@lecheynata.com>
2022-06-26 14:50:17 -0500

    create customer resource

---
+++ app/Http/Resources/CustomerResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'dni' => $this->dni,
+            'email' => $this->email,
+            'name' => $this->name,
+            'last_name' => $this->last_name
+        ];
+    }
+}
