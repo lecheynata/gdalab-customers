5f6217c18f44e195080d1eafcbced49758d1e03a
eduardo mena <dev@lecheynata.com>
2022-06-26 14:40:45 -0500

    create communes migration

---
+++ database/migrations/2022_06_26_193902_create_communes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('communes', function (Blueprint $table) {
+            $table->id('id_com');
+            $table->integer('id_reg');
+            $table->string('description', 90);
+            $table->enum('status', ['A', 'I', 'trash'])->default('A');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('communes');
+    }
+};
