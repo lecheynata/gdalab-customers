fd6bcad5c2efff61d5000ef93829448300ce0f8a
eduardo mena <dev@lecheynata.com>
2022-06-26 23:08:12 -0500

    add relations in customer

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -11,4 +11,21 @@ class Customer extends Model
 
     // Disable timestamps defaults
     public $timestamps = false;
+
+    /**
+     * The relationships that should always be loaded.
+     *
+     * @var array
+     */
+    protected $with = ['commune'];
+
+    public function commune()
+    {
+        return $this->hasOne(Commune::class, 'id_com', 'id_com');
+    }
+
+    public function region()
+    {
+        return $this->hasOne(Region::class, 'id_reg', 'id_reg');
+    }
 }
